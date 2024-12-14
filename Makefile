88483562098be89f8418d13aa5f0d8ac4cb3d026
Casey Tucker <dctucker@hotmail.com>
2024-12-14 01:08:35 -0800

    tested and it kinda works

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	arduino-cli compile --build-path build --fqbn esp32:esp32:m5stack_dial
--- m5dial-midi.ino
+++ m5dial-midi.ino
@@ -2,34 +2,43 @@
 #include <M5Dial.h>
 //#include <M5_SAM2695.h>
 
-#define MidiIn  Serial2
-#define MidiOut Serial3
+#define MIDI_BAUD 31250
+#define MidiIn  Serial1
+#define MidiOut Serial2
 
-void setup() {
-	auto cfg = M5.config();
-	M5Dial.begin(cfg, true, false);
-	M5Dial.Lcd.setTextColor(YELLOW);
-	M5Dial.Lcd.setTextDatum(middle_center);
-	M5Dial.Lcd.setTextFont(&fonts::Orbitron_Light_32);
-	M5Dial.Lcd.setTextSize(2);
-}
-
-long encoder_pos = -999;
+long encoder_pos = 0;
 byte status, channel, cc, data_pos;
+byte shown_channel;
 typedef struct {
 	byte bank_msb, bank_lsb, pgm;
 } ChannelProgram;
 ChannelProgram programs[16];
 
+void setup() {
+	auto cfg = M5.config();
+  MidiIn.begin(MIDI_BAUD); //, SERIAL_8N1);
+  MidiOut.begin(MIDI_BAUD); // , SERIAL_8N1);
+
+	M5Dial.begin(cfg, true, false);
+	M5Dial.Lcd.setTextColor(YELLOW);
+	M5Dial.Lcd.setTextDatum(middle_center);
+	M5Dial.Lcd.setTextFont(&fonts::Orbitron_Light_32);
+	M5Dial.Lcd.setTextSize(1);
+  shown_channel = 0;
+}
+
 void loop() {
 	M5Dial.update();
 
 	bool dirty;
-	if (MidiIn.available()) {
-		byte input = MidiIn.read();
+  byte input;
+	while (MidiIn.available()) {
+		input = MidiIn.read();
+    //M5Dial.Speaker.tone(2000, 20);
+    ///*
 		if (input & 0x80) { // status
-			status = input && 0xf0;
-			channel = input && 0x0f;
+			status = input & 0xf0;
+			channel = input & 0x0f;
 			data_pos = 0;
 			dirty = false;
 		} else { // data
@@ -39,7 +48,7 @@ void loop() {
 				case 0xa0: // key pressure
 					break;
 				case 0xb0: // controller
-					dirty = true;
+					//dirty = true;
 					switch(data_pos) {
 						case 0:
 							cc = input;
@@ -73,30 +82,38 @@ void loop() {
 					break;
 				default:;
 			}
-		}
-	}
-
-	if (dirty) {
-		auto s = "CH" + String(channel, DEC) + ": "
-			+ String(programs[channel].bank_msb) + " "
-			+ String(programs[channel].bank_lsb) + " "
-			+ String(programs[channel].pgm);
-		M5Dial.Lcd.drawString(s, M5Dial.Lcd.width()/2, M5Dial.Lcd.height()/2);
+		} //*/
 	}
 
 	long newpos = M5Dial.Encoder.read();
-	if (newpos != encoder_pos) {
-		M5Dial.Speaker.tone(8000, 20);
-		M5Dial.Lcd.clear();
+  if (newpos != encoder_pos) {
+    if (newpos % 4 == 0) {
+  		M5Dial.Speaker.tone(8000, 20);
+			shown_channel += 16 + (newpos - encoder_pos);
+			shown_channel %= 16;
+			dirty = true;
+    }
 		encoder_pos = newpos;
-		M5Dial.Lcd.drawString(String(encoder_pos), M5Dial.Lcd.width()/2, M5Dial.Lcd.height()/2);
+		//M5Dial.Lcd.drawString(String(encoder_pos), M5Dial.Lcd.width()/2, M5Dial.Lcd.height()/2);
 	}
 	if (M5Dial.BtnA.wasPressed()) {
 		M5Dial.Encoder.readAndReset();
+    shown_channel = 0;
 	}
 	if (M5Dial.BtnA.pressedFor(5000)) {
 		M5Dial.Encoder.write(100);
-		M5Dial.Speaker.tone(4000, 50);
+		M5Dial.Speaker.tone(2000, 25);
+	}
+
+	if (dirty) {
+		M5Dial.Lcd.clear();
+		auto p = programs[shown_channel];
+		auto s1 = "CH " + String(shown_channel + 1, DEC);
+		auto s2 = String(p.bank_msb + 1, DEC) + ":"
+			+ String(p.bank_lsb + 1, DEC) + ":"
+			+ String(p.pgm + 1, DEC);
+		M5Dial.Lcd.drawString(s1, M5Dial.Lcd.width()/2, M5Dial.Lcd.height()/2 - 24);
+		M5Dial.Lcd.drawString(s2, M5Dial.Lcd.width()/2, M5Dial.Lcd.height()/2 + 24);
 	}
 }
 
